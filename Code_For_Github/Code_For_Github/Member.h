13d633e2a50f38cfb159fb2401c9c28e26c6e9c2
Hakan Cil <22428805@bucks.ac.uk>
2026-03-09 17:18:43 +0000

    Commented Member.h

--- Code_For_Github/Code_For_Github/Member.h
+++ Code_For_Github/Code_For_Github/Member.h
@@ -3,21 +3,38 @@
 #include "User.h"
 #include <vector>
 
+//Forward declaration to avoid circular dependency
 class Book;
 
 class Member : public User {
 private:
+
+	//Stores pointers to books currently borrowed by this member
 	std::vector<Book*> borrowedBooks;
+
+	//Maximum number of books a member can borrow
 	const int MAX_LIMIT = 5;
 
 public:
 	Member(std::string n);
+
+	//Overrides the pure virtual getRole() function from User
 	std::string getRole() const override;
+
+	//Checks if the member is allowed to borrow another book
 	bool canBorrow() const;
+
+	//Adds a book to the member's borrowed list
 	void addBorrowedBook(Book* book);
+
+	//Removes a returned book from the member's borrowed list
 	void removeBorrowedBook(Book* book);
+
+	//Returns how many books the member has currently borrowed
 	int getBorrowCount() const;
 	void viewBorrowedBooks() const;
+
+	//Allows admin to update the member's name
 	void setName(const std::string& newName);
 };
 

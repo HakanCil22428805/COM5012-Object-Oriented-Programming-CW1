72bb9ead588982ab702ba54e1e701b5cd23ebc81
Hakan Cil <22428805@bucks.ac.uk>
2026-03-09 16:42:47 +0000

    Commented Admin.cpp

--- Code_For_Github/Code_For_Github/Admin.cpp
+++ Code_For_Github/Code_For_Github/Admin.cpp
@@ -1,7 +1,11 @@
 #include "Admin.h"
 
+//Admin constructor
+//The colon syntax callsthe base class constructor (User) and passes the name
 Admin::Admin(std::string n) : User(n) {}
 
+//Returns the role of this user
+//Used in the system to determine permissions and menu access
 std::string Admin::getRole() const {
 	return "Admin";
 }

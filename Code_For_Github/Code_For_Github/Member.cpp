f83d7ab67a8e71ecf39c4c29957c6a4e22c7b690
Hakan Cil <22428805@bucks.ac.uk>
2026-03-09 17:24:24 +0000

    Commented Member.cpp

--- Code_For_Github/Code_For_Github/Member.cpp
+++ Code_For_Github/Code_For_Github/Member.cpp
@@ -8,17 +8,24 @@ std::string Member::getRole() const {
 	return "Member";
 }
 
+//Returns true if the member has borrowed fewer than the maximum allowed
 bool Member::canBorrow() const {
 	return borrowedBooks.size() < MAX_LIMIT;
 }
 
+//Adds a book pointer to the borrowedBooks vector
 void Member::addBorrowedBook(Book* book) {
 	borrowedBooks.push_back(book);
 }
 
 void Member::removeBorrowedBook(Book* book) {
+	//Loop through the borrowed books
 	for (int i = 0; i < borrowedBooks.size(); i++) {
+
+		//Check if the current book matches the one being returned
 		if (borrowedBooks[i] == book) {
+
+			//Removes the book
 			borrowedBooks.erase(borrowedBooks.begin() + i);
 			return;
 		}
@@ -30,6 +37,8 @@ int Member::getBorrowCount() const {
 }
 
 void Member::viewBorrowedBooks() const {
+
+	//If the member has no books borrowed
 	if (borrowedBooks.empty()) {
 		std::cout << "You have not borrowed any books.\n";
 		return;
@@ -37,7 +46,10 @@ void Member::viewBorrowedBooks() const {
 
 	std::cout << "\nYour Borrowed Books.\n";
 
+	//Loop through each borrowed book
 	for (Book* b : borrowedBooks) {
+
+		//Displays title and author
 		std::cout << "Title: " << b->getTitle()
 			<< " | Author: " << b->getAuthor()
 			<< "\n";

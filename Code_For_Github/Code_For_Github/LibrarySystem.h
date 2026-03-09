20005eb671e87214e6d3755671322f5bf1619ae9
Hakan Cil <22428805@bucks.ac.uk>
2026-03-09 17:28:15 +0000

    Commented LibrarySystem.h

--- Code_For_Github/Code_For_Github/LibrarySystem.h
+++ Code_For_Github/Code_For_Github/LibrarySystem.h
@@ -7,8 +7,12 @@
 #include "User.h"
 #include "Member.h"
 
+//LibrarySystem class manages the entire library
+//It stores books and users and controls system operations
 class LibrarySystem {
 private:
+
+	//Vector storing pointers to all books and users in the library
 	std::vector<Book*> books;
 	std::vector<User*> users;
 
@@ -16,19 +20,27 @@ public:
 	LibrarySystem();
 	~LibrarySystem();
 
+	//Book management functions
 	void addBook(std::string title, std::string author);
 	void removeBook(std::string title);
 
+	//Search functions
 	Book* searchByTitle(std::string title);
 	std::vector<Book*>searchByAuthor(std::string author);
 
+	//Book viewing functions
+	//Used by members
 	void viewBooksBasic() const;
+
+	//Used by librarians
 	void viewBooksDetailed() const;
 
+	//Borrowing system
 	void borrowBook(Member* member, std::string title);
 	void reserveBook(Member* member, std::string title);
 	void returnBook(Member* member, std::string title);
 
+	//User management
 	bool addUser(User* user);
 	User* findUser(const std::string name);
 	void listUsers() const;

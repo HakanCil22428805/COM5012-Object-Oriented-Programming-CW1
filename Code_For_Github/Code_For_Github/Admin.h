1cbf8ff36b3d4ece5e084c5f90dd9d10658f0869
Hakan Cil <22428805@bucks.ac.uk>
2026-03-09 16:32:36 +0000

    Commented Admin.h

--- Code_For_Github/Code_For_Github/Admin.h
+++ Code_For_Github/Code_For_Github/Admin.h
@@ -1,11 +1,17 @@
+//Header guard start. This prevents the file from being included multiple times
 #ifndef ADMIN_H
 #define ADMIN_H
 #include "User.h"
 
 class Admin : public User {
 public:
+	//constructor that takes a name and passes it to the User constructor
 	Admin(std::string n);
+
+	//Overrides the pure virtual getRole() function from the User class
+	//This allows polymorphism when checking user roles
 	std::string getRole() const override;
 };
 
 #endif
+// End of header guard

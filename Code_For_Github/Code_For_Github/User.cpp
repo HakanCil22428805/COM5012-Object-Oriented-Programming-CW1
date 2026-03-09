7d10f36accdf1abb632d1d586d4f54a77909f29a
Hakan Cil <22428805@bucks.ac.uk>
2026-03-09 16:57:53 +0000

    Commented User.cpp

--- Code_For_Github/Code_For_Github/User.cpp
+++ Code_For_Github/Code_For_Github/User.cpp
@@ -1,12 +1,21 @@
 #include "User.h"
 
+//Constructor for User
+//Initialises the name variable using an initialiser list
 User::User(std::string n) : name(n) {}
+
+//Virtual destructor
+//It ensures derived classes are destroyed correctly
 User::~User() {}
 
+//Returns the name of the user
 std::string User::getName() const {
 	return name;
 }
 
+//Allows the name of the user to be updated
+//Used when the admin edits user details
 void User::setName(const std::string& newName) {
+	//Assigns the new name to the name variable
 	name = newName;
 }
